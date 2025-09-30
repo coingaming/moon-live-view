14c2c859bd663d62db152b587b38aebff7802111
MarioGranada <mario.hernandez@yolo.com>
2025-09-30 16:48:22 +0300

    feat: MDS-1960 table updates

--- apps/moon_live_view/lib/moon_live_view/components/table.ex
+++ apps/moon_live_view/lib/moon_live_view/components/table.ex
@@ -1,31 +1,83 @@
 defmodule MoonLiveView.Table do
   use MoonLiveView.Component
 
-  attr :rows, :list, default: [], doc: "List of table rows"
   attr :size, :string, values: ~w"sm md lg xl", default: "md", doc: "Size of the table"
   attr :class, :string, default: "", doc: "Additional CSS classes for the table"
   attr :rest, :global, doc: "Additional HTML attributes for the table"
 
-  slot :col, doc: "List of table items" do
-    attr :class, :string, doc: "Label of the table item"
-  end
-
-  slot :row, doc: "List of table rows" do
-    attr :class, :string, doc: "Additional CSS classes for the table row"
-  end
-
   def table(assigns) do
     ~H"""
-    <table class={["moon-table", get_size(@size), @class]} {@rest}>
-      <thead>
-        <th :for={col <- @col} class={col[:class] || ""}>{render_slot(col)}</th>
-      </thead>
-      <tbody>
-        <tr :for={row <- @row} class={row[:class] || ""}>
-          {render_slot(row)}
-        </tr>
-      </tbody>
+    <table class={join(["moon-table", get_size(@size), @class])} {@rest}>
+      {render_slot(@inner_block)}
     </table>
+    """
+  end
+
+  attr :class, :string, default: "", doc: "Additional CSS classes for the table head"
+  attr :rest, :global, doc: "Additional HTML attributes for the table head"
+
+  slot :inner_block, doc: "Inner content of the table head"
+
+  def table_head(assigns) do
+    ~H"""
+    <thead class={@class} {@rest}>
+        {render_slot(@inner_block)}
+    </thead>
+    """
+  end
+
+  attr :class, :string, default: "", doc: "Additional CSS classes for the table head cell"
+  attr :rest, :global, doc: "Additional HTML attributes for the table head cell"
+
+  slot :inner_block, doc: "Inner content of the table head cell"
+
+  def table_head_cell(assigns) do
+    ~H"""
+    <th class={@class} {@rest}>{render_slot(@inner_block)}</th>
+    """
+  end
+
+  attr :class, :string, default: "", doc: "Additional CSS classes for the table body"
+  attr :rest, :global, doc: "Additional HTML attributes for the table body"
+
+  slot :inner_block, doc: "Inner content of the table body"
+
+  def table_body(assigns) do
+    ~H"""
+    <tbody class={@class} {@rest}>{render_slot(@inner_block)}</tbody>
+    """
+  end
+
+  attr :class, :string, default: "", doc: "Additional CSS classes for the table footer"
+  attr :rest, :global, doc: "Additional HTML attributes for the table footer"
+
+  slot :inner_block, doc: "Inner content of the table footer"
+
+  def table_footer(assigns) do
+    ~H"""
+    <tfoot class={@class} {@rest}>{render_slot(@inner_block)}</tfoot>
+    """
+  end
+
+  attr :class, :string, default: "", doc: "Additional CSS classes for the table caption"
+  attr :rest, :global, doc: "Additional HTML attributes for the table caption"
+
+  slot :inner_block, doc: "Inner content of the table caption"
+
+  def table_caption(assigns) do
+    ~H"""
+    <caption class={@class} {@rest}>{render_slot(@inner_block)}</caption>
+    """
+  end
+
+  attr :class, :string, default: "", doc: "Additional CSS classes for the table row"
+  attr :rest, :global, doc: "Additional HTML attributes for the table row"
+
+  slot :inner_block, doc: "Inner content of the table row"
+
+  def table_row(assigns) do
+    ~H"""
+    <tr class={@class} {@rest}>{render_slot(@inner_block)}</tr>
     """
   end
 
@@ -36,7 +88,7 @@ defmodule MoonLiveView.Table do
 
   def table_cell(assigns) do
     ~H"""
-    <td class={["moon-table-cell", @class]} {@rest}>{render_slot(@inner_block)}</td>
+    <td class={@class} {@rest}>{render_slot(@inner_block)}</td>
     """
   end
 
--- apps/moon_live_view_docs/lib/moon_live_view_docs_web/storybook/content_display/table.story.exs
+++ apps/moon_live_view_docs/lib/moon_live_view_docs_web/storybook/content_display/table.story.exs
@@ -4,7 +4,8 @@ defmodule Storybook.Components.CoreComponents.Table do
 
   def function, do: &Table.table/1
 
-  def imports, do: [{Table, [table_cell: 1]}]
+  def imports,
+    do: [{Table, [table_head: 1, table_head_cell: 1, table_body: 1, table_row: 1, table_cell: 1]}]
 
   def variations do
     [
@@ -13,24 +14,30 @@ defmodule Storybook.Components.CoreComponents.Table do
         description: "Default",
         slots: [
           """
-          <:col>Title</:col>
-          <:col>Title</:col>
-          <:col>Title</:col>
-          <:row>
-            <.table_cell>Cell</.table_cell>
-            <.table_cell>Cell</.table_cell>
-            <.table_cell>Cell</.table_cell>
-          </:row>
-          <:row>
-            <.table_cell>Cell</.table_cell>
-            <.table_cell>Cell</.table_cell>
-            <.table_cell>Cell</.table_cell>
-          </:row>
-          <:row>
-            <.table_cell>Cell</.table_cell>
-            <.table_cell>Cell</.table_cell>
-            <.table_cell>Cell</.table_cell>
-          </:row>
+          <.table_head>
+            <.table_row>
+              <.table_head_cell>Title</.table_head_cell>
+              <.table_head_cell>Title</.table_head_cell>
+              <.table_head_cell>Title</.table_head_cell>
+            </.table_row>
+          </.table_head>
+          <.table_body>
+            <.table_row>
+              <.table_cell>Cell</.table_cell>
+              <.table_cell>Cell</.table_cell>
+              <.table_cell>Cell</.table_cell>
+            </.table_row>
+            <.table_row>
+              <.table_cell>Cell</.table_cell>
+              <.table_cell>Cell</.table_cell>
+              <.table_cell>Cell</.table_cell>
+            </.table_row>
+            <.table_row>
+              <.table_cell>Cell</.table_cell>
+              <.table_cell>Cell</.table_cell>
+              <.table_cell>Cell</.table_cell>
+            </.table_row>
+          </.table_body>
           """
         ]
       }
