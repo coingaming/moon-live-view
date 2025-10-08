d9abeffb4e97da60010f063fc6032b88837a65dc
MarioGranada <mario.hernandez@yolo.com>
2025-10-08 17:59:02 +0300

    feat: MDS-1960 more updates

--- apps/moon_live_view/mix.exs
+++ apps/moon_live_view/mix.exs
@@ -4,7 +4,7 @@ defmodule MoonLiveView.MixProject do
   def project do
     [
       app: :moon_live_view,
-      version: "1.0.0-beta.0",
+      version: "1.0.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
